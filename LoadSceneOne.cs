08981529a21c0e7c3dfbf9e8ef87e6d54e495198
Adrian J. Fox <themage107@gmail.com>
2018-11-01 16:52:59 -0700

    Show the floor moving like at an airport. Only using the material though.
    Mark your 3D object as static. Use this offset feature to save
    performance on the model.

---
+++ LoadSceneOne.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneOne : MonoBehaviour {                                         
+
+    // Use this for initialization
+    void Start () {
+
+        //set camera constraints with camera script
+        levelConstants.setCameraConstraints(7.5f, -11.8f);
+
+        //bring in the level constants: pause menu, initial overlay, johnny, camera identification
+        this.gameObject.AddComponent<levelConstants>();
+
+        levelConstants.johhnyPosOvrd = true;
+        levelConstants.johnnyXOvrd = -14.5f;
+        levelConstants.johnnyYOvrd = -2.76f;
+        levelConstants.johnnyZOvrd = -0.5f; 
+	}                                                          
+    
+}
